d984cb016ae325eacb50ef05fdb014985db6944a
sshikhar99 <sshikhar494@gmail.com>
2026-02-11 00:22:59 +0530

    Initial commit: Luminorr Energy static website

---
+++ js/include.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("[data-include]").forEach(el => {
+    fetch(el.getAttribute("data-include"))
+      .then(res => res.text())
+      .then(data => el.innerHTML = data);
+  });
+});
